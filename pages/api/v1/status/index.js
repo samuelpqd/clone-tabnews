33735dcdd8fc673c694f22504863a01df4c2fba7
Samuel Motta <samuel@apoiotatico.com.br>
2023-10-29 20:47:51 +0000

    add `compose` config file with database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,7 @@
 function status(request, response) {
-  response.status(200).json({ chave: "Os alunos são acima da média!" });
+  response
+    .status(200)
+    .json({ chave: "Os alunos do curso.dev são acima da média!" });
 }
 
 export default status;
